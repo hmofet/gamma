815c3f1027f7b32d6fc08d9443776f8a98562d54
hmofet <rmbakht@yahoo.ca>
2017-03-05 11:54:43 -0500

    Web stuff

---
+++ gamma_web_php_components/update.php
@@ -0,0 +1,18 @@
+<html><body><?php
+
+if (isset($POST['csv']){
+	$content = $_POST['csv']; 
+	$file = "eeg.csv"; 
+	$file_to_save = fopen($file, 'a'); 
+	fwrite($file_to_save, $content); 
+	fclose($file_to_size);
+	echo $_POST['csv'];
+} else if (isset($POST['mmse']){
+	$content = $_POST['mmse']; 
+	$file = "mmse.csv"; 
+	$file_to_save = fopen($file, 'a'); 
+	fwrite($file_to_save, $content); 
+	fclose($file_to_size);
+	echo $_POST['mmse'];
+}
+?></body></html>
